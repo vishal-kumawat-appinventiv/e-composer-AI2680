4de2b65049292ad414388eefd287bc18a4d9efeb
vishal-kumawat-appinventiv <vishal.kumawat@appinventiv.com>
2024-09-29 02:58:41 +0530

    📦 DropDown added in nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,21 @@
+import { useNavigate } from "react-router-dom";
 import { LOGO_IMG } from "../utils/mock";
+import React from "react";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  const handleCategoryChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    const selectedValue = event.target.value;
+    if (selectedValue === "1") {
+      navigate("/");
+    } else if (selectedValue === "2") {
+      navigate("/contact");
+    }
+  };
+
   return (
     <section className="sticky top-0 z-50 flex items-center justify-between w-full p-3 bg-[#f1f1f1] border border-b">
       <div className="flex items-center gap-2">
@@ -8,7 +23,15 @@ const Navbar = () => {
         <h1 className="text-2xl font-bold">EComposer</h1>
       </div>
       <div className="hidden lg:flex items-center gap-3">
-        <h3 className="text-sm text-black">Lifestyle Blog Listing</h3>
+        <select
+          name="category"
+          id="category"
+          className="border border-black rounded bg-white border-none outline-none px-2 py-1"
+          onChange={handleCategoryChange}
+        >
+          <option value="1">Blog Listing</option>
+          <option value="2">Contact Us</option>
+        </select>
         <input
           type="text"
           placeholder="Type something to find template"
@@ -19,7 +42,7 @@ const Navbar = () => {
         <button className="text-white bg-black p-1 text-sm rounded-lg px-2">
           Try this demo now
         </button>
-        <button className="text-white bg-black p-1 text-sm rounded-lg px-2  ">
+        <button className="text-white bg-black p-1 text-sm rounded-lg px-2">
           Get app free
         </button>
       </div>
