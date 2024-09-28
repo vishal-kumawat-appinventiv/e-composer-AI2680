297652dfb46d4e180baa3c722e116c6d295078de
vishal-kumawat-appinventiv <vishal.kumawat@appinventiv.com>
2024-09-28 17:52:43 +0530

    📦 heroSection

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,16 +1,37 @@
-import { HERO_IMG_1, HERO_IMG_2 } from "../utils/mock";
+import { HERO_IMG_1, HERO_IMG_2, LOGO_IMG } from "../utils/mock";
 
 const HeroSection = () => {
   return (
     <>
       <section className="p-4 w-full box-border">
-        <div className="grid grid-cols-1 lg:grid-cols-[70%_30%] gap-4 w-full max-w-full overflow-hidden">
-          <div className="col1 max-h-[95vh] rounded-xl overflow-hidden">
+        <div className="grid grid-cols-1 lg:grid-cols-[70%_29%] gap-4 w-full max-w-full overflow-hidden">
+          <div className="relative col1 max-h-[95vh] rounded-xl overflow-hidden">
             <img
               src={HERO_IMG_1}
               alt="hero-img"
-              className="object-cover w-full h-full"
+              className="object-cover w-full h-full -z-10"
             />
+            <div className="absolute bottom-5 lg:bottom-16 left-5 lg:left-16 z-10 flex flex-col justify-center gap-3 lg:gap-8 w-full">
+              <p className="text-white text-sm">FASHION</p>
+              <h1 className="text-white text-3xl lg:text-7xl max-w-[60%]">
+                {
+                  "Here's what's new in your favourite stores & online this week..."
+                }
+              </h1>
+              <div className="flex gap-3">
+                <button className="flex items-center justify-center gap-2 rounded-r-full rounded-l-full py-2 px-4 text-sm font-bold text-white backdrop-blur-sm bg-white/20">
+                  <img
+                    src={LOGO_IMG}
+                    alt="logo"
+                    className="w-8 h-8 rounded-full"
+                  />
+                  Admin Dinh
+                </button>
+                <button className="p-2 px-4 rounded-full border border-white cursor-pointer hover:bg-pink-400">
+                  <i className="bi bi-arrow-up-right text-white"></i>
+                </button>
+              </div>
+            </div>
           </div>
           <div className="col2 flex flex-col justify-center gap-6 h-full w-full min-w-0 overflow-hidden">
             <div className="bg-[#bde48a] flex-grow h-[50%] lg:h-[30%] w-full rounded-xl">
--- src/utils/mock.ts
+++ src/utils/mock.ts
@@ -17,3 +17,5 @@ export const HERO_IMG_1 =
 
 export const HERO_IMG_2 =
   "https://cdn.shopify.com/s/files/1/0726/1221/7115/files/bn-bl-2.png?v=1683619218";
+
+export const LOGO_IMG = "https://cdn.shopify.com/s/files/1/0726/1221/7115/users/avatar_250x250_crop_center_7b84e44b-2e69-463a-83d1-09c0337c268c.png?v=1683616182";
