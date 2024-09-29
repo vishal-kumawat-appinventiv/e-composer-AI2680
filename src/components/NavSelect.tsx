fe4c81e92ee2e4801563211d109b93befde3d488
vishal-kumawat-appinventiv <vishal.kumawat@appinventiv.com>
2024-09-29 22:42:09 +0530

    📦 NavSelect Default value added

--- src/components/NavSelect.tsx
+++ src/components/NavSelect.tsx
@@ -21,6 +21,7 @@ const NavSelect = () => {
       id={Date.now().toString()}
       className="border-2 rounded-lg bg-white outline-none px-2 py-1 cursor-pointer"
       onChange={handleCategoryChange}
+      defaultValue={window.location.pathname === "/" ? "1" : "2"}
     >
       <option value="1">Blog Listing</option>
       <option value="2">Contact Us</option>
